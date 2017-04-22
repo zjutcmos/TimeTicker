b06d19a7a6bdc533fdcc718cb9a6c5b328ecf9ce
zjutcmos <1517633079@qq.com>
2017-04-22 21:14:54 +0800

    初次提交

---
+++ app/src/main/java/com/example/timeticker/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.timeticker;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, MyClockView.DownCountTimerListener {
+    Button mBtn;
+    private MyClockView myClockView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.clock_view_activity);
+        mBtn = (Button) findViewById(R.id.button1);
+        mBtn.setOnClickListener(this);
+        myClockView = (MyClockView) findViewById(R.id.clockView);
+        myClockView.setDownCountTimerListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.button1:
+                myClockView.setDownCountTime(1000L * 60L + 1000L * 12L);
+                myClockView.startDownCountTimer();
+
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void stopDownCountTimer() {
+        Toast.makeText(this,"结束了",Toast.LENGTH_SHORT).show();
+    }
+}
---
+++ app/src/main/java/com/example/timeticker/MyTextView.java
@@ -0,0 +1,42 @@
+package com.example.timeticker;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+public class MyTextView extends TextView {
+
+	Paint mPaint;
+	
+	public MyTextView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		initPaint();
+	}
+
+	public MyTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+		super(context, attrs, defStyleAttr);
+		initPaint();
+	}
+
+	public MyTextView(Context context) {
+		this(context, null);
+		initPaint();
+	}
+	
+	private void initPaint() {
+		mPaint=new Paint(Paint.ANTI_ALIAS_FLAG);
+		mPaint.setStyle(Paint.Style.FILL);
+		mPaint.setColor(Color.BLACK);
+	}
+
+	@Override
+	protected void onDraw(Canvas canvas) {
+		super.onDraw(canvas);
+		canvas.drawRect(0,getMeasuredHeight()/2-1,getMeasuredWidth(),getMeasuredHeight()/2+1, mPaint);
+		
+	}
+
+}
